82d00b8a370f41f268f921d9f21709f4ac749349
fgadeach <f.gadea@uniandes.edu.co>
2021-10-20 11:12:24 -0500

    Test1

---
+++ UDP/Client/cliente.py
@@ -0,0 +1,70 @@
+import socket
+import logging
+from datetime import datetime
+import hashlib
+
+tamanio = 1024
+formato = "utf-8"
+puerto = 6000
+ip = '192.168.80.128'
+direccion = (ip, puerto)
+
+def main():
+    dateTimeObj = datetime.now()
+    logging.basicConfig(filename=f'Logs/{dateTimeObj.year}-{dateTimeObj.month}-{dateTimeObj.day}-{dateTimeObj.hour}-{dateTimeObj.minute}-{dateTimeObj.second}.log', filemode='w', level=logging.DEBUG)
+
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    cliente.connect(direccion)
+
+    data = cliente.recv(tamanio).decode('utf-8')
+    item = data.split("_")
+    nombreArch = item[0]
+    tamanioArchivo = int(item[1])
+    ID = item[2]
+    connectionNumber = item[3]
+    ip = item[4]
+    puerto = item[5]
+
+    logging.info(
+        f"Se recibio el archivo : {nombreArch} con tamaño de {tamanioArchivo}")
+
+    logging.info(
+        f"El cliente {ID}  se encuentra con la direccion: {ip} : {puerto}")
+
+    print("\n Datos del archivo recibido")
+    cliente.send("Datos del archivo recibido".encode(formato))
+
+    numPaquetes = 0
+    tiempoTranferenciaI = datetime.now()
+    with open(f"ArchivosRecibidos/{ID}-Prueba-{connectionNumber}", "w") as f:
+        while True:
+            data = cliente.recv(tamanio).decode(formato)
+
+            if not data:
+                break
+
+            f.write(data)
+            cliente.send("Archivo recibido".encode(formato))
+            numPaquetes += 1
+    tiempoTranferenciaF = datetime.now()
+
+    tiempoTranferencia = tiempoTranferenciaF-tiempoTranferenciaI
+  
+    BUF_SIZE = 1024
+
+    with open(f"Cliente/ArchivosRecibidos/{ID}-Prueba-{connectionNumber}", 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+
+    logging.info(f'Tiempo de transferencia : {tiempoTranferencia}')
+    logging.info(
+        f'Numero de paquetes recibidos fue: {numPaquetes}')
+    logging.info(
+        f"Numero de bytes recibidos fue: {numPaquetes*tamanio}")
+
+    cliente.close()
+
+if __name__ == "__main__":
+    main()
---
+++ UDP/Servidor/servidor.py
@@ -0,0 +1,112 @@
+import socket
+import threading
+import os
+import logging
+from datetime import datetime
+import hashlib
+tamanio = 1024
+
+formato = "utf-8"
+puerto = 6000
+ip = '192.168.80.128'
+direccion = (ip, puerto)
+
+def manejarCliente(conn, addr, nombreArch, tamanioArchivo, ID, connectionNumber, hashServidor, ip, puerto):
+    logging.info(f'Se genero la conexion {ID} desde {ip}:{puerto}')
+
+    print(" Conexion del cliente {addr}: ready")
+
+    data = f"{nombreArch}_{tamanioArchivo}_{ID}_{connectionNumber}_{hashServidor}_{ip}_{puerto}"
+    conn.send(data.encode(formato))
+    mensaje = conn.recv(tamanio).decode(formato)
+    print("Respuesta del cliente: " + mensaje)
+
+    numPaquetesEnviados = 0
+    tiempoTranferenciaI = datetime.now()
+    with open(nombreArch, "r") as f:
+        while True:
+            data = f.read(tamanio)
+
+            if not data:
+                break
+
+            conn.send(data.encode(formato))
+            mensaje = conn.recv(tamanio).decode(formato)
+            numPaquetesEnviados += 1
+    tiempoTranferenciaF = datetime.now()
+
+    tiempoTranferencia = tiempoTranferenciaF-tiempoTranferenciaI
+    logging.info(
+        f'Tiempo de transferencia de cliente {ID} fue: {tiempoTranferencia}')
+
+    logging.info(
+        f'Numero de paquetes enviados al cliente {ID} fueron: {numPaquetesEnviados}')
+    logging.info(
+        f'Numero de bytes enviados al cliente {ID} fueron: {numPaquetesEnviados*tamanio}')
+    conn.close()
+
+def main():
+    dateTimeObj = datetime.now()
+    logging.basicConfig(
+        filename=f"Logs/{dateTimeObj.year}-{dateTimeObj.month}-{dateTimeObj.day}-{dateTimeObj.hour}-{dateTimeObj.minute}-{dateTimeObj.second}.log", level=logging.INFO)
+
+    numValidoConexiones = False
+    while(not numValidoConexiones):
+        numConexiones = input("Ingrese numero de conexiones:")
+        if(int(numConexiones) >= 1 and int(numConexiones) <= 25):
+            numValidoConexiones = True
+        else:
+            print(f"{numConexiones} El numero de conexiones debe de estar entre 1 y 25")
+
+    connectionNumber = int(numConexiones)
+
+    archivoValido = False
+    while(not archivoValido):
+        archivo = input("Ingrese 1 para el archivo de 100MB o ingrese 2 para el archivo de 250MB:")
+        if(int(archivo) == 2):
+            nombreArch = "ArchivosEnvio/250MB.txt"
+            tamanioArchivo = os.path.getsize(nombreArch)
+            archivoValido = True
+        elif(int(archivo) == 1):
+            nombreArch = "ArchivosEnvio/100MB.txt"
+            tamanioArchivo = os.path.getsize(nombreArch)
+            archivoValido = True
+        else:
+            print("Archivo Invalido (Inputs debe ser 1 o 2)")
+
+    ## HASHING ##
+    BUF_SIZE = 1024
+
+    logging.info(f'Nombre del archivo enviado : {nombreArch}')
+    logging.info(f'Tamanio del archivo  : {tamanioArchivo}')
+
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    servidor.bind(direccion)
+    servidor.listen()
+    print("Servidor en standby ....")
+
+    threads = []
+
+    clientsNumber = 0
+
+    
+    while (clientsNumber < connectionNumber):
+        conn, addr = servidor.accept()
+        clientsNumber += 1
+        print(f"Conexion del cliente desde {addr[0]}:{addr[1]}")
+        thread = threading.Thread(target=manejarCliente, args=(
+            conn, addr, nombreArch, tamanioArchivo, clientsNumber, connectionNumber, addr[0], {addr[1]}))
+        threads.append(thread)
+        print(f" Hilos de ejecucion en este momento: {threading.active_count()-1}")
+        print(f"Conexion de clientes: {clientsNumber}")
+
+    i = 0
+    while(i < clientsNumber):
+        threads[i].start()
+        i += 1
+
+    servidor.close()
+
+
+if __name__ == "__main__":
+    main()
