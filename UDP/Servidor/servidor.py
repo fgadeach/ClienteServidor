0183abb755aac619ad49c6d9b403df78b16c6657
fgadeach <f.gadea@uniandes.edu.co>
2021-10-20 14:58:28 -0500

    test

--- UDP/Servidor/servidor.py
+++ UDP/Servidor/servidor.py
@@ -16,8 +16,8 @@ def manejarCliente(conn, addr, nombreArch, tamanioArchivo, ID, connectionNumber,
     print(" Conexion del cliente {addr}: ready")
 
     data = f"{nombreArch}_{tamanioArchivo}_{ID}_{connectionNumber}_{ip}_{puerto}"
-    conn.send(data.encode(formato))
     mensaje = conn.recv(tamanio).decode(formato)
+    conn.sendto(mensaje,addr)
     print("Respuesta del cliente: " + mensaje)
 
     numPaquetesEnviados = 0
@@ -29,8 +29,8 @@ def manejarCliente(conn, addr, nombreArch, tamanioArchivo, ID, connectionNumber,
             if not data:
                 break
 
-            conn.send(data.encode(formato))
             mensaje = conn.recv(tamanio).decode(formato)
+            conn.sendto(mensaje,addr)
             numPaquetesEnviados += 1
     tiempoTranferenciaF = datetime.now()
 
